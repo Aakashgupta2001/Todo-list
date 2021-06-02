9149496c5132d2a9e91a48b2b26c59817d99314d
aakash gupta <aakashgupta2001.2909@gmail.com>
2021-06-02 20:26:49 +0530

    changed the style , made a build conf. at package.json

--- src/App.js
+++ src/App.js
@@ -13,10 +13,12 @@ function App() {
     <div className="main">
       <Form settodo = {settodoInput}  todo={todoInput} settodolist = {settodolist} todolist={todoList} />
       <div className="todoList">
+        <h1 className="todoHeading">Todo List</h1>
       {todoList.map((currentTodo) => (
         <Profile todo = {currentTodo} />
       ))}
       </div>
+      <h6 className="workInProgress">This app is still a work in progress...</h6>
     </div>
   );
 }
