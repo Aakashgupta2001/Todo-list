9cef6d55b9620c973ed99f771f72411dc3ab1b76
aakash gupta <aakashgupta2001.2909@gmail.com>
2021-06-24 17:38:50 +0530

    added remove

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
-import React , {useState} from "react";
+import React, { useState } from "react";
 import "./App.css";
-import Profile from "./testHook";
+import Todo from "./todo";
 import Form from "./form";
 
 function App() {
-
-  const [todoInput , settodoInput] = useState('');
-  const [todoList , settodolist] = useState([])
-
+  const [todoInput, settodoInput] = useState("");
+  const [todoList, settodolist] = useState([]);
 
   return (
     <div className="main">
-      <Form settodo = {settodoInput}  todo={todoInput} settodolist = {settodolist} todolist={todoList} />
+      <Form
+        settodo={settodoInput}
+        todo={todoInput}
+        settodolist={settodolist}
+        todolist={todoList}
+      />
       <div className="todoList">
         <h1 className="todoHeading">Todo List</h1>
-      {todoList.map((currentTodo) => (
-        <Profile todo = {currentTodo} />
-      ))}
+        {todoList.map((currentTodo) => (
+          <Todo
+            todo={currentTodo}
+            todolist={todoList}
+            settodolist={settodolist}
+          />
+        ))}
       </div>
-      <h6 className="workInProgress">This app is still a work in progress...</h6>
+      <h6 className="workInProgress">
+        This app is still a work in progress...
+      </h6>
     </div>
   );
 }
--- src/todo.js
+++ src/todo.js
@@ -1,15 +1,24 @@
 import React, { useState } from "react";
 
-function Profile({ todo }) {
+function Profile({ todo ,settodolist , todolist}) {
   const [count, setCount] = useState(0);
   const increment = () => {
     setCount(count + 1);
   };
+  const remove = () =>{
+    let newtodo =  todolist.filter(etodo => {
+      return etodo.text !== todo.text
+    })
+    console.log(newtodo)
+    settodolist(newtodo)
+  }
+
   return (
     <div className="profile-container">
       <p>{todo.text}</p>
       <p className="priorityCounter">{count}</p>
       <button onClick={increment}>Priority</button>
+      <button onClick={remove}>Remove</button>
     </div>
   );
 }
